433c7fb5bc9d9fd7e22cbcf295987d926c7e1b67
RexLK <1586662742@qq.com>
2022-07-29 20:43:28 +0800

    fix yolov5

--- side_line_learn/yolo5-6.0-ros/src/yolov5-6.0/src/detect.cpp
+++ side_line_learn/yolo5-6.0-ros/src/yolov5-6.0/src/detect.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     cudaSetDevice(0);
 
     std::string engine_name = "yolov5s.engine";
-    yolov5 *det = new yolov5(engine_name, 7);
+    yolov5 *det = new yolov5(engine_name);
 
     cv::Mat img = cv::imread("1.jpg");
     int w = img.cols;
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     cudaMalloc((void **)&d_image, sizeof(unsigned char) * w * h * 3);
     cudaMemcpy(d_image, img.data, w * h * 3 * sizeof(unsigned char),cudaMemcpyHostToDevice);
 
-    det->detect(d_image, w, h,img);
+    bool flag = det->detect(d_image, w, h,img);
 
     cudaFree(d_image);
     return 0;
